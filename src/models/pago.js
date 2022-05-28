989754528d6391182d8adc62111bcc94c9e352fd
zzinue <zzinue@gmail.com>
2022-05-28 12:10:19 -0500

    populated fixed

--- src/models/pago.js
+++ src/models/pago.js
@@ -1,15 +1,14 @@
-
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-     monto: { type: Number, required: true},
-     fecha_pago: { type: Date, required: true},
-     comprobante: { type: Buffer, required: true},
-     aprobado: { type: Boolean, required: true, default: false},
-     residente:{type:String,required:true},
-    
+    monto: { type: Number, required: true },
+    fecha_pago: { type: Date, required: true },
+    comprobante: { type: String, required: true },
+    aprobado: { type: Boolean, required: true, default: false },
+    residente: { type: String, required: true },
+
 });
 
 module.exports = {
--- src/usecases/pagos_configuracion/index.js
+++ src/usecases/pagos_configuracion/index.js
@@ -1,24 +1,31 @@
 const Pagos_configuracion = require("../../models/pagos_configuracion").model;
 
-const get = async () => {
+const get = async() => {
     //devuelve todos los pagos_configuracion
-    return await Pagos_configuracion.find().populate("pagos_configuracion").exec();
+    return await Pagos_configuracion.find().exec();
 
 }
 
-const getById = async (id) => {
-    const pagos_configuracion =  await Pagos_configuracion.findById(id).populate("pagos_configuracion").exec();
+const getById = async(id) => {
+    const pagos_configuracion = await Pagos_configuracion.findById(id).exec();
     return pagos_configuracion;
     //devuelve un pagos_configuracion
 }
 
-const create = async (Pagos_configuracionData) => {
+const create = async(Pagos_configuracionData) => {
 
-    const { concepto,descripcion,monto,periodicidad,tieneFechaLimite,fechaLimite,fecha_registro, permisos } = Pagos_configuracionData;
+    const { concepto, descripcion, monto, periodicidad, tieneFechaLimite, fechaLimite, fecha_registro, permisos } = Pagos_configuracionData;
 
 
     const newPagos_configuracion = new Pagos_configuracion({
-       concepto,descripcion,monto,periodicidad,tieneFechaLimite,fechaLimite,fecha_registro, permisos
+        concepto,
+        descripcion,
+        monto,
+        periodicidad,
+        tieneFechaLimite,
+        fechaLimite,
+        fecha_registro,
+        permisos
     });
 
     const savedPagos_configuracion = await newPagos_configuracion.save();
@@ -26,31 +33,42 @@ const create = async (Pagos_configuracionData) => {
     return savedPagos_configuracion;
 }
 
-const update = async (id, Pagos_configuracionData) => {
+const update = async(id, Pagos_configuracionData) => {
     // actualizar pagos_configuracion
-    const { 
-        concepto,descripcion,monto,periodicidad,tieneFechaLimite,fechaLimite,fecha_registro, permisos } = Pagos_configuracionData;
+    const {
+        concepto,
+        descripcion,
+        monto,
+        periodicidad,
+        tieneFechaLimite,
+        fechaLimite,
+        fecha_registro,
+        permisos
+    } = Pagos_configuracionData;
 
     const updatedPagos_configuracion = await Pagos_configuracion.findByIdAndUpdate(
-        id,
-        {
-            concepto,descripcion,monto,periodicidad,tieneFechaLimite,fechaLimite,fecha_registro, permisos
-        },
-        { new: true }
+        id, {
+            concepto,
+            descripcion,
+            monto,
+            periodicidad,
+            tieneFechaLimite,
+            fechaLimite,
+            fecha_registro,
+            permisos
+        }, { new: true }
     ).exec();
 
     return updatedPagos_configuracion;
 }
 
-const patch = async (id, Pagos_configuracionData) => {
+const patch = async(id, Pagos_configuracionData) => {
     return await Pagos_configuracion.findByIdAndUpdate(
-        id,
-        { ...Pagos_configuracionData },
-        { new: true }
+        id, {...Pagos_configuracionData }, { new: true }
     ).exec();
 };
 
-const del = async (id) => {
+const del = async(id) => {
 
 
     return await Pagos_configuracion.findByIdAndDelete(id).exec();
