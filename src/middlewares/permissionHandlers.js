60ca701c0b4f0c3f6069d650ee2cacf03509993e
Martha Luna <lunam91@hotmail.com>
2022-06-12 20:42:12 -0500

    remove log

--- src/middlewares/permissionHandlers.js
+++ src/middlewares/permissionHandlers.js
@@ -3,10 +3,10 @@ const jwt = require("../lib/jwt");
 const adminHandler = async (req, res, next) => {
   try {
     const { token } = req.headers;
-    console.log("req.params", req.params);
+  
     const verifiedToken = await jwt.verify(token);
     req.params.tokenPayload = verifiedToken;
-    console.log(req.params.tokenPayload);
+    
     const { role } = req.params.tokenPayload;
 
     if (role !== "admin") {
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -37,7 +37,7 @@ router.post("/login", async (req, res, next) => {
     }
     const retrievedAdmin = await admin.getAdminByUser(email)
     if (retrievedAdmin) {
-      console.log(retrievedAdmin);
+      
 
 
 
