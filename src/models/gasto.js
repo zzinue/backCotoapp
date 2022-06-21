2c69246dd6cccc853cee0780737326511704b077
zzinue <zzinue@gmail.com>
2022-06-20 19:58:11 -0500

    gasto fixed

--- src/models/gasto.js
+++ src/models/gasto.js
@@ -1,17 +1,17 @@
-    const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-    const Schema = mongoose.Schema;
-    
-    const schema = new Schema({
-         concepto: { type: String, required: true},
-         monto: { type: Number, required: true},
-         fecha_gasto: { type: Date, required: true},
-         comprobante: { type: Buffer, required: true},
-         descripcion:{type:String,required:false},
-         imagen_mejora: { type:Buffer, required: false},
-    });
-    
-    module.exports = {
-        schema,
-        model: mongoose.model("Gastos", schema),
-    }
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    concepto: { type: String, required: true },
+    monto: { type: Number, required: true },
+    fecha_gasto: { type: Date, required: true },
+    comprobante: { type: String, required: true },
+    descripcion: { type: String, required: false },
+    imagen_mejora: { type: Buffer, required: false },
+});
+
+module.exports = {
+    schema,
+    model: mongoose.model("Gastos", schema),
+}
