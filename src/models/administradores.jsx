296d4b0dc65db526cae17af96b8b7fd787a819fb
zzinue <zzinue@gmail.com>
2022-07-03 02:30:18 -0500

    adding administradores model

---
+++ src/models/administradores.jsx
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    nombre: { type: String, required: true },
+    email: { type: String, required: true },
+    nombreCoto: { type: String, required: true },
+    logo: { type: String, required: true },
+});
+
+schema.methods.toJSON = function () {
+    let administrador = this.toObject();
+    delete administrador.password;
+    return administrador;
+}
+module.exports = {
+    schema,
+    model: mongoose.model("Administrador", schema),
+}
